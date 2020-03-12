ea1aa519556e72d3fa3118edeac6a6c079dfb78f
Dudiko2 <dudiko2@gmail.com>
2020-03-12 20:30:29 +0200

    procfile

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 8080;
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "build", "index.html"));
+	});
+}
 
 app.get("/", (req, res) => {
 	res.send("hey");
